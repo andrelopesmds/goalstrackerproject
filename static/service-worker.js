2ef15ec9062a11d0a3b07ed7c4f0859c22331260
andrelms1 <andre.marinho@engenharia.ufjf.br>
2018-03-03 22:54:18 -0300

    add default icon to push message

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,7 +2,12 @@ self.addEventListener('push', function(event) {
   if (event.data) {
     console.log('This push event has data: ', event.data.text());
 
-    const promiseChain = self.registration.showNotification(event.data.text());
+    const options = {
+	"icon": 'images/ball.png'
+				
+};
+
+    const promiseChain = self.registration.showNotification(event.data.text(), options);
     event.waitUntil(promiseChain);
 
   } else {
