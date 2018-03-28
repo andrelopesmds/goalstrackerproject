97faa29d95b63dc8a3f50890ac18c769c37e5d10
andrelopesmds <andrelopesmds@gmail.com>
2018-03-28 11:24:18 -0300

    update tests

--- node/test/fetch.test.js
+++ node/test/fetch.test.js
@@ -12,24 +12,15 @@ describe('Fetch service', function(){
 
   describe('should have a config message method', function(){
 
-    it('should be a function', function(){
+    it('which is a function', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.configMessage, 'function');
     })
    
-    it('should return a string' , function(){
-       assert.equal(typeof C.configMessage(game) , 'string');
-    })
-    
-    it("should return title message", function(){  
+    it('should return the correct string' , function(){
+      assert.equal(typeof C.configMessage(game) , 'string');
       assert.equal(JSON.parse(C.configMessage(game)).title, 'Gol do Atletico Mineiro!');
-    })
-
-    it("should return body message", function(){
       assert.equal(JSON.parse(C.configMessage(game)).body, 'Atletico Mineiro 3-1 Cruzeiro');
-    })
-
-    it("should return icon path", function(){
       assert.equal(JSON.parse(C.configMessage(game)).icon, 'images/ball.png');
     })
 
