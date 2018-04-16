72871847b41a0a3bf9295b02312e4761a4608940
andrelopesmds <andrelopesmds@gmail.com>
2018-04-16 17:10:36 -0300

    update push

--- node/push/push.js
+++ node/push/push.js
@@ -11,29 +11,29 @@ controlDB.connect(path.join(appPath, '../app/db'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/', function(req, res) {
 
-    Promise.all([
-        validateRequest(req),
-        getSubscriptionsFromDatabase()
-    ])
-    .then(function (data) {
-        var message = data[0];
-        var subscriptions = data[1];
-        
-        sendMsg(subscriptions, message);
+    var message = validateRequest(req);
 
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({'success': true}));
-        
-    })
-    .catch(function (err){
-        // bad request
+    if(message) {
+        return getSubscriptionsFromDatabase()
+        .then(function (subscriptions) {
+            sendMsg(subscriptions, message);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({'success': true}));
+        })
+        .catch(function (err){
+            // server error, can't get subscriptions or send messages
+            console.log(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500)
+            res.send({'success': false});
+        })
+    } else {
+        // invalid request - one of the required fields is empty
         res.setHeader('Content-Type', 'application/json');
         res.status(400)
         res.send({'success': false});
-        console.log("Error: "+ err);
-    })
-  
-
+    }
 })
 
 app.listen(3000, 'localhost')
@@ -51,43 +51,10 @@ webpush.setVapidDetails(
 );
 
 
-function validateRequest(req){
-    // check if the request has the fields required: title, body, icon
-    return new Promise(function (resolve, reject) {
-        var obj = JSON.parse(req.body.message);
-        if (obj.title && obj.body && obj.icon) {
-            resolve(JSON.stringify(obj));
-        } else {
-            reject("The request does not have the required fields");
-            console.log(req.body);
-        }
-    });
-}
-
-
-function triggerPushMsg(subscription, dataToSend) {
-    return webpush.sendNotification(subscription, dataToSend)
-        .catch((err) => {
-            if (err.statusCode === 410) {
-                return removeSubscriptionFromDatabase(subscription);
-
-            } else {
-                console.log('Subscription is no longer valid: ', err);
-
-            }
-        });
-};
-
-function getSubscriptionsFromDatabase(callback, message) {
-    return new Promise(function (resolve, reject){   
-        controlDB.getUsers(function(subscriptions) {
-            resolve(subscriptions);
-        })
-    });
-}
 
 function sendMsg(data, message) {
 
+    count = 0;
     for (let i = 0; i < data.length; i++) {
 
         const subscription = {
@@ -99,15 +66,44 @@ function sendMsg(data, message) {
             }
         }
 
-        triggerPushMsg(subscription, message);
+        webpush.sendNotification(subscription, message)
+        .catch((err) => {
+            if (err.statusCode === 410) {
+                return removeSubscriptionFromDatabase(subscription);
+            } else {
+                console.log('Subscription is no longer valid: ', err);
+            }
+        })
+       
     }
 }
 
-function removeSubscriptionFromDatabase(subscription) {
-
-    controlDB.removeSubscription(subscription);
-
+function getSubscriptionsFromDatabase(callback, message) {
+    return new Promise(function (resolve, reject){   
+        controlDB.getUsers(function(subscriptions) {
+            resolve(subscriptions);
+        })
+    });
 }
+
+function removeSubscriptionFromDatabase(subscription) {
+    controlDB.removeSubscription(subscription);
+}
+
+function validateRequest(req){
+    // check if the request has the fields required: title, body, icon
+    if (req.body.message) {
+       var obj = JSON.parse(req.body.message);
+       if (obj.title && obj.body && obj.icon) {
+           return JSON.stringify(obj);
+       } else {
+           return null;
+       }
+    } else {
+        return null;
+    }
+}
+
 
 var push = {};
 push.app = app;
