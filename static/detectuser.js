1f305620c807f54334bf796df70849c97f27fb21
andrelopesmds <andrelopesmds@gmail.com>
2018-06-30 10:35:13 -0300

    improve UX

--- static/detectuser.js
+++ static/detectuser.js
@@ -2,12 +2,7 @@ function detectUser() {
     var permission = Notification.permission;
     if (permission === "granted") {
         document.getElementById("p1").style.display = "none";
-        document.getElementById("p2").style.display = "";
-        document.getElementById("p3").style.display = "";
-
-    } else {
-        document.getElementById("p1").style.display = "";
-        document.getElementById("p2").style.display = "none";
-        document.getElementById("p3").style.display = "none";
+        document.getElementById("p2").style.display = "block";
+        document.getElementById("p3").style.display = "inline-block";
     }
 }
