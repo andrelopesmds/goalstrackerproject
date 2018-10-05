867c0189c978b37c5a1f880974e572e4c7bce287
andrelopesmds <andrelopesmds@gmail.com>
2018-10-04 21:52:05 -0300

    Make Siga o galo alive again

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -8,32 +8,21 @@ var url = 'http://localhost:3000';
 
 var interceptor = nock(url).post('/').reply(200, {'success' : true});
 
-var game = {
-    team1 : 'Atletico Mineiro',
-    team2 : 'Cruzeiro',
-    score : '3-1',
-    currentStatus : 'Goal for Atletico Mineiro!'
+var match = {
+    game : 'Atletico Mineiro 2 x 0 Cruzeiro',
+    competition : 'Copa do brasil',
+    live : true
 }
 
-var games = [ game ];
+var matches = [ match ];
 
 describe('Fetch service', function() {
-    describe('checkTeam function', function() {
-        it('should return true if the team matches the string , ', function() {
-            assert.equal(typeof fetch.checkTeam, 'function');
-            assert.equal(fetch.checkTeam('Atletico Mineiro'), true);
-            assert.equal(fetch.checkTeam('Atletico-MG'), true);
-            assert.equal(fetch.checkTeam('Atletico-PR'), false);
-        })
-    })
-
     describe('runApi function', function() {
         this.timeout(5000);
 
         it('should return an array with the data of the matches', async () => {
-            const result = await fetch.runApi();
-            result[0].should.include.keys('team1', 'team2', 'score',
-                                          'currentStatus');
+            const result = await fetch.matches('brazil', 'atletico-mineiro')
+            result[0].should.include.keys('game', 'live', 'competition');
         });
     })
 
@@ -41,12 +30,12 @@ describe('Fetch service', function() {
         it('should return the correct string in Portuguese', function() {
             assert.equal(typeof fetch, 'object');
             assert.equal(typeof fetch.configMessage, 'function');
-            assert.equal(typeof fetch.configMessage(game), 'string');
-            assert.equal(JSON.parse(fetch.configMessage(game)).title,
-                         'Gol do Atletico Mineiro!');
-            assert.equal(JSON.parse(fetch.configMessage(game)).body,
-                         'Atletico Mineiro 3-1 Cruzeiro');
-            assert.equal(JSON.parse(fetch.configMessage(game)).icon,
+            assert.equal(typeof fetch.configMessage(match), 'string');
+            assert.equal(JSON.parse(fetch.configMessage(match)).title,
+                         'Atletico Mineiro 2 x 0 Cruzeiro');
+            assert.equal(JSON.parse(fetch.configMessage(match)).body,
+                         'Copa do brasil');
+            assert.equal(JSON.parse(fetch.configMessage(match)).icon,
                          'images/ball.png');
         })
     })
@@ -55,9 +44,8 @@ describe('Fetch service', function() {
         it('should return a string if there is a new status in the game and null otherwise',
            function() {
                assert.equal(typeof fetch.checkGameStatus, 'function');
-               assert.equal(fetch.checkGameStatus(),
-                            null); // invalid entry data
-               assert.equal(typeof fetch.checkGameStatus(games), 'string');
+               assert.equal(fetch.checkGameStatus(), null);
+               assert.equal(typeof fetch.checkGameStatus(matches), 'string');
            })
     })
 
