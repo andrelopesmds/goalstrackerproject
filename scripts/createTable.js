9b2b33f96da5e12159d2f3ec6245f7169ad99ed8
andrelopesmds <andrelopesmds@gmail.com>
2018-11-19 15:16:11 -0200

    indent correction

--- scripts/createTable.js
+++ scripts/createTable.js
@@ -1,29 +1,25 @@
 var AWS = require("aws-sdk");
 
-AWS.config.update({
-    region: "us-east-1"
-});
+AWS.config.update({region : "us-east-1"});
 
 var dynamodb = new AWS.DynamoDB();
 
 var params = {
     TableName : "Visitors",
-    KeySchema: [       
-        { AttributeName: "endpoint", KeyType: "HASH"}  //Partition key
+    KeySchema : [
+        {AttributeName : "endpoint", KeyType : "HASH"} // Partition key
     ],
-    AttributeDefinitions: [       
-        { AttributeName: "endpoint", AttributeType: "S" }
-    ],
-    ProvisionedThroughput: {       
-        ReadCapacityUnits: 2,
-        WriteCapacityUnits: 2
-    }
+    AttributeDefinitions :
+        [ {AttributeName : "endpoint", AttributeType : "S"} ],
+    ProvisionedThroughput : {ReadCapacityUnits : 2, WriteCapacityUnits : 2}
 };
 
 dynamodb.createTable(params, function(err, data) {
     if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+        console.error("Unable to create table. Error JSON:",
+                      JSON.stringify(err, null, 2));
     } else {
-        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+        console.log("Created table. Table description JSON:",
+                    JSON.stringify(data, null, 2));
     }
 });
