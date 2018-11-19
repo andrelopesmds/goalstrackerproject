a6b4f06206383474c3a6c08f4319b21028c43349
andrelopesmds <andrelopesmds@gmail.com>
2018-11-19 21:16:47 -0200

    Use AWS keys as env variables.

--- node/app/controldb.js
+++ node/app/controldb.js
@@ -2,7 +2,11 @@ var AWS = require("aws-sdk");
 
 const tableName = "Visitors";
 
-AWS.config.update({region : "us-east-1"});
+AWS.config.update({
+    region : "us-east-1",
+    accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
+});
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
