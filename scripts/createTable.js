7a470544428a53372b394610fb287e04058b27ec
andrelopesmds <andrelopesmds@gmail.com>
2018-11-19 21:21:15 -0200

    Using aws keys as env variable.

--- scripts/createTable.js
+++ scripts/createTable.js
@@ -1,7 +1,10 @@
 var AWS = require("aws-sdk");
 
-AWS.config.update({region : "us-east-1"});
-
+AWS.config.update({
+    region : "us-east-1",
+    accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
+});
 var dynamodb = new AWS.DynamoDB();
 
 var params = {
