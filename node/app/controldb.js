6e3ab4f7947c2e7124e3501f226d7397af7e33dc
andrelopesmds <andrelopesmds@gmail.com>
2018-11-19 15:16:11 -0200

    indent correction

--- node/app/controldb.js
+++ node/app/controldb.js
@@ -2,45 +2,40 @@ var AWS = require("aws-sdk");
 
 const tableName = "Visitors";
 
-AWS.config.update({
-    region: "us-east-1"
-});
+AWS.config.update({region : "us-east-1"});
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.insert = function(endpoint, expirationTime, key256, keyAuth) {
+exports.insert =
+    function(endpoint, expirationTime, key256, keyAuth) {
     var date = new Date();
     var dateISO = date.toISOString();
 
     var params = {
-        TableName: tableName,
-        Item: {
-            "endpoint": endpoint,
-            "expirationTime": expirationTime,
-            "key256": key256,
-            "keyAuth": keyAuth,
-            "subscribeDate": dateISO,
-            "unsubscribeDate": null
+        TableName : tableName,
+        Item : {
+            "endpoint" : endpoint,
+            "expirationTime" : expirationTime,
+            "key256" : key256,
+            "keyAuth" : keyAuth,
+            "subscribeDate" : dateISO,
+            "unsubscribeDate" : null
         }
     };
 
     docClient.put(params, function(err, data) {
-        if(err)
+        if (err)
             throw err;
         console.log("1 visitor inserted!");
-    }); 
+    });
 }
 
-exports.getSubscriptionDates = function(callback) {
-    var params = {
-        TableName: tableName
-    };
-
+    exports.getSubscriptionDates = function(callback) {
+    var params = {TableName : tableName};
 
     docClient.scan(params, function(err, data) {
-        if(err)
+        if (err)
             throw err;
         callback(data.Items);
     });
 }
-
