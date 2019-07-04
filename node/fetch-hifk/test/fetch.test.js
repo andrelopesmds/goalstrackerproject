6e63895a177b2ccb87539950ab585cc106c6a8bc
andrelopesmds <andrelopesmds@gmail.com>
2019-07-05 00:43:05 +0300

    Setup unit tests for all services.

--- node/fetch-hifk/test/fetch.test.js
+++ node/fetch-hifk/test/fetch.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('test', function() {
   describe('#test', function() {
-    it('app1 tested ok', function() {
+    it('fetch-hifk tested ok', function() {
       assert.equal(3, 3);
     });
   });
---
+++ node/push/test/push.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('test', function() {
+  describe('#test', function() {
+    it('push tested ok', function() {
+      assert.equal(3, 3);
+    });
+  });
+});
