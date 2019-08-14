328fa0a27b198a3fa6156e950041634cef8da3be
André L. M. dos Santos <31055728+andrelopesmds@users.noreply.github.com>
2019-08-14 22:43:35 +0300

    Update subscription-validator.js

--- node/app/subscription-validator.js
+++ node/app/subscription-validator.js
@@ -3,7 +3,7 @@ exports.isValid = function(data) {
         data.endpoint && typeof data.endpoint === 'string' && data.endpoint.includes('https') &&
         data.keys && typeof data.keys === 'object' &&
         data.keys.p256dh && typeof data.keys.p256dh === 'string' &&
-        data.keys.auth && typeof data.keys.auth === 'string' &&
+        data.keys.auth && typeof data.keys.auth === 'string'
     ) return true;
 
     return false;
