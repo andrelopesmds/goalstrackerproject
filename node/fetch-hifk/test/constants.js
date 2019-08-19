aa0ec7feaad80c36531a740e53276d8eb3708ff4
andrelopesmds <andrelopesmds@gmail.com>
2019-08-19 22:32:48 +0300

    Unit tests for fetch-hifk.

---
+++ node/fetch-hifk/test/constants.js
@@ -0,0 +1,22 @@
+constants = {
+    matches: {},
+    team: {}
+};
+
+constants.matches.finished = [
+    {
+        team1: 'HIFK HELSINKI',
+        team2: 'Pelicans',
+        score: '3-1',
+        currentStatus: 'Match finished'
+    }
+];
+
+constants.team.corrects = [
+    'HIFK HELSINKI',
+    'IFK HELSINKI',
+    'hifk helsinki',
+    'ifk helsinki'
+];
+
+module.exports = constants;
