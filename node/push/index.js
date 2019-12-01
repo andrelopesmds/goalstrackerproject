277c4f5227503b9cb257b4141ec5d6c3d42814ad
andrelopesmds <andrelopesmds@gmail.com>
2019-12-02 00:53:16 +0200

    Save state. Push messsage sent sucessfully.

--- node/push/index.js
+++ node/push/index.js
@@ -7,7 +7,30 @@ const vapidKeys = {
     privateKey: process.env.privateKey
 };
 
+webpush.setVapidDetails(
+    `mailto:sigaogalo@gmail.com`,
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+
 module.exports.handler = async (event) => {
+    try {
+        await sendPushNotification(event);
+    } catch (error) {
+        console.log(`Error when sending push notification: ${JSON.stringify(error)}`);
+        throw error;
+    }
     console.log('This is working');
     console.log(JSON.stringify(event));
 };
+
+async function sendPushNotification(event) {
+    let message = {
+        title: event.obj.score,
+        body: event.obj.team1
+    }
+    var payload = JSON.stringify(message);
+
+    const result = await webpush.sendNotification(event.subscription, payload);
+    console.log(result);
+}
