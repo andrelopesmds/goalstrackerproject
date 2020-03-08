56aaab5422cf49c21886469b77e5b1e83dd85d30
andrelopesmds <andrelopesmds@gmail.com>
2020-03-08 23:37:23 +0200

    Include subscription validation.

--- node/subscriber/index.js
+++ node/subscriber/index.js
@@ -2,22 +2,56 @@
 
 const dynamodb = require('../lib/dynamodb');
 
-module.exports.handler = async (event) => {
-  const body = JSON.parse(event['body']);
+const SUCCESS_RESPONSE = {
+  statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
+  body: JSON.stringify({
+    message: `User subscribed!`,
+  }),
+};
 
+module.exports.handler = async (event) => {
   try {
-    await dynamodb.saveSubscription(body);
+    await saveSubscription(event);
   } catch (error) {
     console.log(`Error when subscribing new user: ${JSON.stringify(error)}`);
     throw error;
   }
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify({message: `User subscribed!`, body: body}),
-  };
+  return SUCCESS_RESPONSE;
 };
+
+async function saveSubscription(event) {
+  const body = JSON.parse(event['body']);
+  if (!isSubscriptionValid(body)) {
+    throw new Error('Subscription is not valid');
+  }
+
+  await dynamodb.saveSubscription(body);
+}
+
+function isSubscriptionValid(body) {
+  if (checkProperties(body, ['endpoint', 'keys']) && checkProperties(body.keys, ['p256dh', 'auth'])) {
+    return true;
+  }
+
+  if (!body.endpoint.includes('https')) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkProperties(obj, propertiesList) {
+  for (let i = 0; i < propertiesList.length; i++) {
+    if (!obj.hasOwnProperty(propertiesList[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
