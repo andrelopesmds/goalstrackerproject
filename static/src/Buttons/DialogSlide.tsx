c6d0489acfae54a60eb6d6bfe551a6375be87404
andrelopesmds <andrelopesmds@gmail.com>
2020-09-25 01:02:57 +0300

    Fix autocomplete bug - was selecting wrong team after click on dropdown menu.

--- static/src/Buttons/DialogSlide.tsx
+++ static/src/Buttons/DialogSlide.tsx
@@ -9,9 +9,12 @@ import { TransitionProps } from '@material-ui/core/transitions';
 import CheckboxesTags from './CheckboxesTags';
 import { AvailableTeam } from '../Utils/globalInterfaces';
 
-const Transition = React.forwardRef<unknown, TransitionProps>(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+  ) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 interface DialogSlideProps {
   onClick: Function,
