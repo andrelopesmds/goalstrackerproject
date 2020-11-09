a0fa0117240b1bdb33318a02eaed1d193efb598c
andrelopesmds <andrelopesmds@gmail.com>
2020-11-10 00:29:12 +0200

    Avoid duplicates in list of matches.

--- node/test/fetch/helper.test.js
+++ node/test/fetch/helper.test.js
@@ -18,4 +18,51 @@ describe('Fetch - Helper test', function() {
 
     assert.equal(JSON.stringify(result), JSON.stringify(constants.LIVE_RESULTS_EVENT_FORMAT));
   });
+
+
+  it('getEventsStandardTeamsNamesAndIds should return only one event for both teams when they play agaisnt each other (no duplicates)', function() {
+    const LIVE_RESULTS = [
+      {
+        "team1": "Atlético Mineiro",
+        "team2": "Santos",
+        "score":"2 - 1"
+      }
+    ];
+    const AVAILABLE_TEAMS = [
+      {
+        "adapterOutputName": "Santos",
+        "id": "99",
+        "adapterInputName": "Santos",
+        "name": "Santos",
+        "country": "brazil",
+        "sport": "football"
+      },
+      {
+        "adapterOutputName": "Atlético Mineiro",
+        "id": "13",
+        "adapterInputName": "Atletico-Mineiro",
+        "name": "Atlético MG",
+        "country": "brazil",
+        "sport": "football"
+      }
+    ];
+    const expected = [
+      {
+        team1: 'Atlético MG',
+        team2: 'Santos',
+        score: '2 - 1',
+        team2Id: '99',
+        team1Id: '13'
+      }
+    ];
+
+    const result = helper.getEventsStandardTeamsNamesAndIds(LIVE_RESULTS, AVAILABLE_TEAMS);
+    assert.equal(1, result.length);
+    assert.equal(expected[0].team1, result[0].team1);
+    assert.equal(expected[0].team1Id, result[0].team1Id);
+    assert.equal(expected[0].team2, result[0].team2);
+    assert.equal(expected[0].team2Id, result[0].team2Id);
+  });
+
+
 });
