bc2c7a44535d06f748dbbd1f17ca2be963c50b3e
andrelopesmds <andrelopesmds@gmail.com>
2021-04-05 14:01:32 -0300

    Script to fetch most recent events from dev Events table.

---
+++ node/dev_scripts/get_most_recent_events.js
@@ -0,0 +1,32 @@
+// This script logs the most recent events from dev Events table
+
+const N_EVENTS_TO_LOG = 10;
+
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1",
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+const paramsProd = {
+  TableName: "dev-EventsTable",
+};
+
+docClient.scan(paramsProd, (err, data) => {
+  if (err) console.log(err);
+
+  var allItems = [];
+  data.Items.forEach(item => {
+    allItems.push(item);
+  });
+
+  allItems = allItems.sort((a, b) => {
+    a.timestamp - b.timestamp;
+  }).reverse();
+
+  for (let i = 0; i < N_EVENTS_TO_LOG; i++) {
+    console.log(allItems[i]);
+  }
+});
