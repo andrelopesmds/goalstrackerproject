1884dc1d4059b537680e17ae11931a19051c81df
andrelopesmds <andrelopesmds@gmail.com>
2021-04-26 16:03:57 -0300

    Added icon/button to close push notification.

--- react/public/sw.js
+++ react/public/sw.js
@@ -1,22 +1,37 @@
-self.addEventListener('push', function(event) {
+const closeAction = 'close-action';
+
+self.addEventListener('push', (event) => {
     const iconPath = 'ball.png';
     const badgePath = 'ball.png';
 
     if (event.data) {
-        let notification = JSON.parse(event.data.text());
-
-        let title = notification.title;
+        const notification = JSON.parse(event.data.text());
+        const { title, body } = notification;
 
         const options = {
-            "badge": badgePath,
-            "body": notification.body,
-            "icon": iconPath // get it from backend in order to customize the message
+            badge: badgePath,
+            body,
+            icon: iconPath, // todo - get it from backend in order to customize the message
+            actions: [
+                {
+                    action: closeAction,
+                    title: 'Close',
+                    icon: iconPath,
+                },
+            ],
         };
 
         const promiseChain = self.registration.showNotification(title, options);
         event.waitUntil(promiseChain);
 
     } else {
-        console.log('This push event has no data.');
+        console.log(`This push event has no data. Event: ${JSON.stringify(event)}.`);
+    }
+});
+
+self.addEventListener('notificationclick', (event) => {
+    const clickedNotification = event.notification;
+    if (event.action === closeAction) {
+        clickedNotification.close();
     }
 });
